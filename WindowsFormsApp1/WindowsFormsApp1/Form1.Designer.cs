a4d6ae0c9c3a2854e501357896bc0288a4544270
nay493 <mohammednayeem45@gmail.com>
2018-02-15 23:23:17 +0530

    Added basic project

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OkButton = new System.Windows.Forms.Button();
+            this.tb_Name = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_Age = new System.Windows.Forms.TextBox();
+            this.tb_ContactNo = new System.Windows.Forms.TextBox();
+            this.tb_Aim = new System.Windows.Forms.TextBox();
+            this.bt_add = new System.Windows.Forms.Button();
+            this.bt_delete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OkButton
+            // 
+            this.OkButton.Location = new System.Drawing.Point(95, 220);
+            this.OkButton.Name = "OkButton";
+            this.OkButton.Size = new System.Drawing.Size(75, 23);
+            this.OkButton.TabIndex = 0;
+            this.OkButton.Text = "OK";
+            this.OkButton.UseVisualStyleBackColor = true;
+            this.OkButton.Click += new System.EventHandler(this.OkButton_Click);
+            // 
+            // tb_Name
+            // 
+            this.tb_Name.Location = new System.Drawing.Point(154, 12);
+            this.tb_Name.Multiline = true;
+            this.tb_Name.Name = "tb_Name";
+            this.tb_Name.Size = new System.Drawing.Size(343, 22);
+            this.tb_Name.TabIndex = 1;
+            this.tb_Name.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Age";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(71, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Contact No";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(71, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Aim";
+            // 
+            // tb_Age
+            // 
+            this.tb_Age.Location = new System.Drawing.Point(154, 60);
+            this.tb_Age.Multiline = true;
+            this.tb_Age.Name = "tb_Age";
+            this.tb_Age.Size = new System.Drawing.Size(343, 22);
+            this.tb_Age.TabIndex = 6;
+            // 
+            // tb_ContactNo
+            // 
+            this.tb_ContactNo.Location = new System.Drawing.Point(154, 108);
+            this.tb_ContactNo.Multiline = true;
+            this.tb_ContactNo.Name = "tb_ContactNo";
+            this.tb_ContactNo.Size = new System.Drawing.Size(343, 22);
+            this.tb_ContactNo.TabIndex = 7;
+            // 
+            // tb_Aim
+            // 
+            this.tb_Aim.AllowDrop = true;
+            this.tb_Aim.AutoCompleteCustomSource.AddRange(new string[] {
+            "Gain Abs",
+            "Cardio",
+            "Loose Weight",
+            "Gain Weight",
+            "Arms"});
+            this.tb_Aim.Location = new System.Drawing.Point(154, 149);
+            this.tb_Aim.Multiline = true;
+            this.tb_Aim.Name = "tb_Aim";
+            this.tb_Aim.Size = new System.Drawing.Size(343, 22);
+            this.tb_Aim.TabIndex = 8;
+            // 
+            // bt_add
+            // 
+            this.bt_add.Location = new System.Drawing.Point(193, 220);
+            this.bt_add.Name = "bt_add";
+            this.bt_add.Size = new System.Drawing.Size(75, 23);
+            this.bt_add.TabIndex = 9;
+            this.bt_add.Text = "ADD";
+            this.bt_add.UseVisualStyleBackColor = true;
+            this.bt_add.Click += new System.EventHandler(this.bt_add_Click);
+            // 
+            // bt_delete
+            // 
+            this.bt_delete.Location = new System.Drawing.Point(288, 219);
+            this.bt_delete.Name = "bt_delete";
+            this.bt_delete.Size = new System.Drawing.Size(75, 23);
+            this.bt_delete.TabIndex = 10;
+            this.bt_delete.Text = "DELETE";
+            this.bt_delete.UseVisualStyleBackColor = true;
+            this.bt_delete.Click += new System.EventHandler(this.bt_delete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(721, 325);
+            this.Controls.Add(this.bt_delete);
+            this.Controls.Add(this.bt_add);
+            this.Controls.Add(this.tb_Aim);
+            this.Controls.Add(this.tb_ContactNo);
+            this.Controls.Add(this.tb_Age);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_Name);
+            this.Controls.Add(this.OkButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OkButton;
+        private System.Windows.Forms.TextBox tb_Name;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb_Age;
+        private System.Windows.Forms.TextBox tb_ContactNo;
+        private System.Windows.Forms.TextBox tb_Aim;
+        private System.Windows.Forms.Button bt_add;
+        private System.Windows.Forms.Button bt_delete;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OkButton_Click(object sender, EventArgs e)
+        {
+            OkButton.Text = tb_Name.Text;
+            tb_Name.Text = "Noway out";
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+        public void CreateFileOrFolder()
+        {
+
+
+           
+    }
+
+        public void bt_add_Click(object sender, EventArgs e)
+        {
+            // Specify a name for your top-level folder.
+            string folderName = @"C:\Users\h122817\Desktop\nay";
+
+            // To create a string that specifies the path to a subfolder under your 
+            // top-level folder, add a name for the subfolder to folderName.
+            string pathString = System.IO.Path.Combine(folderName, "userdata");
+
+            // You can extend the depth of your path if you want to.
+            //pathString = System.IO.Path.Combine(pathString, "SubSubFolder");
+
+            // Create the subfolder. You can verify in File Explorer that you have this
+            // structure in the C: drive.
+            //    Local Disk (C:)
+            //        Top-Level Folder
+            //            SubFolder
+            System.IO.Directory.CreateDirectory(pathString);
+
+            // Create a file name for the file you want to create. 
+            //string fileName = System.IO.Path.GetRandomFileName();
+
+            // This example uses a random string for the name, but you also can specify
+            // a particular name.
+            string fileName = tb_Name.Text+".txt";
+
+            // Use Combine again to add the file name to the path.
+            pathString = System.IO.Path.Combine(pathString, fileName);
+
+            // Verify the path that you have constructed.
+            Console.WriteLine("Path to my file: {0}\n", pathString);
+
+            // Check that the file doesn't already exist. If it doesn't exist, create
+            // the file and write integers 0 - 99 to it.
+            // DANGER: System.IO.File.Create will overwrite the file if it already exists.
+            // This could happen even with random file names, although it is unlikely.
+            if (!System.IO.File.Exists(pathString))
+            {
+               // using (System.IO.File fs = System.IO.File.Create(pathString))
+                //{
+
+                   // fs.Write(tb_Name.Text,0,0);
+                System.IO.File.WriteAllText(pathString, tb_Name.Text+", ");
+                System.IO.File.AppendAllText(pathString,tb_ContactNo.Text + ", ");
+                System.IO.File.AppendAllText(pathString,tb_Age.Text+ ", ");
+                System.IO.File.AppendAllText(pathString,tb_Aim.Text);
+
+               
+
+                //}
+            }
+
+            else
+            {
+                Console.WriteLine("File \"{0}\" already exists.", fileName);
+                return;
+            }
+
+            // Read and display the data from your file.
+            try
+            {
+                byte[] readBuffer = System.IO.File.ReadAllBytes(pathString);
+                foreach (byte b in readBuffer)
+                {
+                    Console.Write(b + " ");
+                }
+                Console.WriteLine();
+            }
+            catch (System.IO.IOException e1)
+            {
+                Console.WriteLine(e1.Message);
+            }
+
+            // Keep the console window open in debug mode.
+           // System.Console.WriteLine("Press any key to exit.");
+            //System.Console.ReadKey();
+        }
+
+        public void bt_delete_Click(object sender, EventArgs e)
+        {
+            System.IO.File.Delete(@"C:\Users\h122817\Desktop\nay\userdata\nay.txt");
+        }
+        // Sample output:
+
+        // Path to my file: c:\Top-Level Folder\SubFolder\ttxvauxe.vv0
+
+        //0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29
+        //30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56
+        // 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 8
+        //3 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99
+
+
+    }
+}
+    
+
+
+
